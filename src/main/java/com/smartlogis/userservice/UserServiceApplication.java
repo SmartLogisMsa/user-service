dda63d3eaf0856a52a958bef553f7db869e309df
hyeon48165 <hyeon48615@gmail.com>
2025-11-19 09:41:50 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/smartlogis/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartlogis.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
