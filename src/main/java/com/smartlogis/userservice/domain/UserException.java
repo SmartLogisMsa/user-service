07f33d2d34fdcf2a67934d9bbe86340753de87f1
hyeon48165 <hyeon48615@gmail.com>
2025-11-19 15:35:47 +0900

    feat: UserRole 구현 #1

---
+++ src/main/java/com/smartlogis/userservice/domain/UserException.java
@@ -0,0 +1,18 @@
+package com.smartlogis.userservice.domain;
+
+import com.smartlogis.common.exception.AbstractException;
+import com.smartlogis.common.exception.MessageCode;
+
+public class UserException extends AbstractException {
+	public UserException(MessageCode messageCode) {
+		super(messageCode);
+	}
+
+	public UserException(MessageCode messageCode, String message) {
+		super(messageCode, message);
+	}
+
+	public UserException(MessageCode messageCode, Throwable cause) {
+		super(messageCode, cause);
+	}
+}
