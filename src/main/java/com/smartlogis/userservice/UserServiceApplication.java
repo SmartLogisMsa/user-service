7b3c2408cd299a583ca0d95dca076db59723a2a6
hyeon48165 <hyeon48615@gmail.com>
2025-11-21 17:19:10 +0900

    fix: jpafactory 관련 오류 수정 #5

--- src/main/java/com/smartlogis/userservice/UserServiceApplication.java
+++ src/main/java/com/smartlogis/userservice/UserServiceApplication.java
@@ -3,8 +3,12 @@ package com.smartlogis.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+
+import com.smartlogis.common.config.QueryDslConfig;
 
 @EnableFeignClients
+@Import(QueryDslConfig.class)
 @SpringBootApplication
 public class UserServiceApplication {
 
