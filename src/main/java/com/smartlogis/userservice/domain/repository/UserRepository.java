68161bbb89a7f4c72238d0e5a79918ed2d176e59
hyeon48165 <hyeon48615@gmail.com>
2025-11-22 22:48:27 +0900

    feat: 조회 Query 추가 #7

--- src/main/java/com/smartlogis/userservice/domain/repository/UserRepository.java
+++ src/main/java/com/smartlogis/userservice/domain/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.smartlogis.userservice.domain.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import com.smartlogis.userservice.domain.User;
@@ -9,6 +10,8 @@ import com.smartlogis.userservice.domain.UserId;
 
 public interface UserRepository extends Repository<User, UserId>, UserRepositoryCustom {
 	User save(User user);
+	@Query("SELECT u FROM User u WHERE u.id = :userId AND u.deletedAt IS NULL")
 	Optional<User> findById(UserId userId);
+	@Query("SELECT u FROM User u WHERE u.username = :username AND u.deletedAt IS NULL")
 	Optional<User> findByUsername(String username);
 }
