03abfbd565cdb6f51ac6f7466815e70d3ae6dcc7
hyeon48165 <hyeon48615@gmail.com>
2025-11-27 21:17:36 +0900

    fix: 로그아웃 오류 수정 #11
    - logout 시, login 리다이렉트 차단

---
+++ src/main/java/com/smartlogis/userservice/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.smartlogis.userservice.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.smartlogis.common.infrastructure.security.HeaderAuthenticationFilter;
+import com.smartlogis.common.infrastructure.security.RestAccessDeniedHandler;
+import com.smartlogis.common.infrastructure.security.RestAuthenticationEntryPoint;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final HeaderAuthenticationFilter headerAuthenticationFilter;
+	private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+	private final RestAccessDeniedHandler restAccessDeniedHandler;
+
+	@Bean
+	@ConditionalOnMissingBean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+			.addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.exceptionHandling(c -> {
+				c.authenticationEntryPoint(restAuthenticationEntryPoint);
+				c.accessDeniedHandler(restAccessDeniedHandler);
+			})
+			.build();
+	}
+
+}
